c147fd76179e5168ccd0f03198605f654f36e9c3
Administrator <Administrator@yuan-PC>
2016-08-06 18:57:27 +0800

    tos

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class IndexController extends MyController {
     public function index(){
         echo 1111;
         $this->display('index');
