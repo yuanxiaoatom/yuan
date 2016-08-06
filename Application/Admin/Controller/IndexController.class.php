33653d094c14196163abfdf69a4504791953d0cb
tang <Administrator@PC-20150818SKMT>
2016-08-06 11:54:29 +0800

    Change-Id: I74b9f4fd25638d5ab6c64bc9c78be449f3b40f91

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller{
+	public function index(){
+		$this->display();
+	}
+	public function drag(){
+		$this->display();
+	}
+	public function left(){
+		$this->display();
+	} 
+	public function main(){
+		$this->display();
+	}
+	public function top(){
+		$this->display();
+	}
+	
+}
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //定义项目层序路径
-define('APP_PATH', './App/');
+define('APP_PATH', './Application/');
 //开启调试模式
 define('APP_DEBUG', true);
 //让入口文件直路由到Admin模块里面的Index控制器里面的index方法
