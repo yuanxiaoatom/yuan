2bce01687a960f29b7f9543556e03ea21d9718d2
tang <Administrator@PC-20150818SKMT>
2016-08-06 11:11:39 +0800

    Change-Id: Ia6b6fbafaf26cc2d4169e6480747415f36754008

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-
-// 应用入口文件
-
-// 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
-
-// 定义应用目录
-define('APP_PATH','./Application/');
-
-// 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
-
-// 亲^_^ 后面不需要任何代码了 就是如此简单
+//定义项目层序路径
+define('APP_PATH', './App/');
+//开启调试模式
+define('APP_DEBUG', true);
+//让入口文件直路由到Admin模块里面的Index控制器里面的index方法
+$_GET['m']='Admin';
+$_GET['c']='Index';
+$_GET['a']='index';
+//引入入口文件
+require 'ThinkPHP/ThinkPHP.php';
